dd98134ab7b8134997a198b9c8926fcd73777326
Gates Wang <gateswang00@gmail.com>
2020-09-26 17:34:50 -0400

    We now print the contents of each token

--- tokenize.c
+++ tokenize.c
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int starts_with(char * prefix, char * string);
-int tokenize(char * string);
-int run_tests(FILE * f);
+typedef enum _type {NONE, SPACE, WORD, DECIMAL, OCTAL, HEX, FLOAT} type;
 
-int main(int argc, char * argv[]){
+int run_tests(FILE *f);
+int tokenize(char *string);
+int starts_with(char *prefix, char *string);
+void set_type(char *input, type *previous, type *current, int *i);
+void print_type(type type);
+
+int main(int argc, char *argv[]){
 	if (argc < 2){
 		printf("Please enter string to tokenize.\n");
 		return -1;
@@ -17,12 +21,12 @@ int main(int argc, char * argv[]){
 		return -1;
 	}
 
-	/*
+/*
 	int length = strlen(argv[1]);
 	char* input = malloc(length+1 * sizeof(char));
 	strcpy(input,argv[1]);
 	return tokenize(input);
-	*/
+*/
 
 	FILE *fptr = fopen(argv[1], "r");
 	run_tests(fptr);
@@ -30,17 +34,114 @@ int main(int argc, char * argv[]){
 	return 0;
 }
 
+
+/*
+	returns 0 if tokenizes string succesfully.
+	returns -1 otherwise.
+*/
+int tokenize(char *input){
+	int length = strlen(input);
+
+	type previous = NONE;
+	type current = NONE;
+	for (int i = 0; i<length ; i++){
+		if(isspace(input[i])){
+			current = SPACE;
+		}
+		else{
+			set_type(input, &previous, &current, &i);
+			if(previous!=current){
+				print_type(current);
+				if(current==HEX)
+					printf("0");
+			}
+			printf("%c", input[i]);
+		}
+		previous = current;
+	}
+	return 0;
+}
+
+/*
+	input: the input string, an index, and the previous type
+	output: the type of the current index
+*/
+void set_type(char *input, type *previous, type *current, int *i){
+	int j = *i;
+	char c = input[j];
+
+	if(starts_with("0x", &input[j]) || starts_with("0X", &input[j])){
+		*current = HEX;
+		*previous = NONE; 
+		(*i)++; //skip the X or x
+	}
+	else if(*previous!=WORD &&
+			*previous!=HEX &&
+			*previous!=DECIMAL &&
+			starts_with("0", &input[j])){
+		*current = OCTAL;
+		*previous = NONE;
+	}
+	else if(isalpha(c)){
+		if(*previous==HEX && isxdigit(c)){
+		}
+		else{
+			*current = WORD;
+		}
+	}
+	else if(isdigit(c)){
+		if( (*previous==HEX && isxdigit(c)) || *previous==WORD || *previous==OCTAL){
+		}
+		else{
+			*current = DECIMAL;
+		}
+	}
+}
+
+/*
+	given a type print the corresponding string
+*/
+void print_type(type type){
+	printf("\n");
+	switch(type){
+		case WORD :
+			printf("word");
+			break;
+		case DECIMAL :
+			printf("decimal");
+			break;
+		case OCTAL :
+			printf("octal");
+			break;
+		case HEX :
+			printf("hex");
+			break;
+		case FLOAT :
+			printf("float");
+			break;
+	}
+	printf(" ");
+}
+
+/*
+	returns true if string begins with prefix
+*/
+int starts_with(char * prefix, char * string){
+    return strncmp(prefix, string, strlen(prefix)) == 0;
+}
+
 /*
 	given a file pointer read the text that is in double quotes
 */
-int run_tests(FILE * f){
+int run_tests(FILE *f){
 	int c;
 	while(c != EOF){
 		//discard text before opening quotation
 		while ((c = fgetc(f)) != '\"' && c != EOF){
 		}
-		char * input = malloc(1000);
-		size_t n = 0;	
+		//read until closing quotation
+		char *input = malloc(1000);
+		size_t n = 0;
 		while ((c = fgetc(f)) != '\"' && c != EOF){
 			input[n++] = (char) c;
 		}
@@ -51,66 +152,3 @@ int run_tests(FILE * f){
 		}
 	}
 }
-
-/*
-	returns 0 if tokenizes string succesfully. 
-	returns -1 otherwise. 
-*/
-int tokenize(char * input){
-	int length = strlen(input);
-	typedef enum _type {NONE, SPACE, WORD, DECIMAL, OCTAL, HEX, FLOAT} type;
-
-	type previous = NONE;
-	type current = NONE;
-	for (int i = 0; i < length ; i++){
-  		char c = input[i];
-		if(isspace(c)){
-			current = SPACE;
-		}
-		else{
-			if(starts_with("0x", &input[i]) || starts_with("0X", &input[i])){
-				current = HEX;
-				previous = NONE;
-				i++; //skip the X or x
-			}
-			else if(previous!=WORD &&
-					previous!=HEX &&
-					previous!=DECIMAL &&
-					starts_with("0", &input[i])){
-				current = OCTAL;
-				previous = NONE;
-			}
-			else if(isalpha(c)){
-				if(previous==HEX && isxdigit(c)){
-				}
-				else{
-					current = WORD;
-				}
-			}
-			else if(isdigit(c)){
-				if( (previous==HEX && isxdigit(c)) || previous==WORD || previous==OCTAL){
-				}
-				else{
-					current = DECIMAL;
-				}
-			}
-			if(previous!=current){
-				if(current==WORD) printf("word\n");
-				if(current==DECIMAL) printf("decimal\n");
-				if(current==HEX) printf("hex\n");
-				if(current==OCTAL) printf("octal\n");
-			}
-		}
-		previous = current;
-	}
-	return 0;
-}
-
-
-int starts_with(char * prefix, char * string)
-{
-    return strncmp(prefix, string, strlen(prefix)) == 0;
-}
-
-
-
